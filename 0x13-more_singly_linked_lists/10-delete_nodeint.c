5ed27364a3958e1b5dc1d692e5ec76927b58e04c
Idaradav <idaradavid878@gmail.com>
2022-10-25 02:06:17 -0700

    3-add_nodeint_en,9-insert_nodeint,10-delete_nodein,100-reverse_list,101-print_listin,102-free_listint.103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,62 @@
+node++)
+	{
+		if (copy->next == NULL)
+			return (-1);
+
+		copy = copy->next;
+	}
+
+	tmp = copy->next;
+	copy->next = tmp->next;
+	free(tmp);
+	return (1);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"10-delete_nodeint.c" [New File]                                                    0,0-1         All
+
